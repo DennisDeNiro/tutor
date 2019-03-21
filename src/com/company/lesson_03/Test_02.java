f7ef0790d58c5312fe60e280d13240faadffbedb
DennisDeNiro <popsmusics@ukr.net>
2019-03-21 12:30:14 +0200

    Init

--- src/com/company/lesson_03/Test_02.java
+++ src/com/company/lesson_03/Test_02.java
@@ -2,4 +2,28 @@ package com.company.lesson_03;
 
 public class Test_02
 {
+    public static void main(String[] args) {
+        System.out.println(plus(15,124));
+        System.out.println(minus(21,34));
+        System.out.println(umnojit(34,2));
+        System.out.println(delit(8,4));
+    }
+
+    public static int plus(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public static int minus(int number1, int number2){
+        return number1 - number2;
+    }
+
+    public static int umnojit(int number1, int number2){
+        return number1 * number2;
+    }
+
+    public static int delit(int number1, int number2){
+        return number1 / number2;
+    }
+
+
 }
---
+++ src/com/company/lesson_03/Test_03.java
@@ -0,0 +1,28 @@
+package com.company.lesson_03;
+/*
+1) Создать метод convert, который должен конвертировать гривны в доллары. В метод должен приходить один аргумент (к-во гривен). Метод должен возвращать к-во долларов.
+2) Создать метод percent, который принимает один аргумент (число) и возвращает это же число + 10% от него.
+3) Создать метод print, который принимает один аргумент (строку) и выводит эту строку 4 раза в консоль.
+*/
+public class Test_03 {
+    public static void main(String[] args) {
+        System.out.println((convert(300.32)));
+        System.out.println(percent(6));
+        print("Text");
+    }
+
+    public static double convert(double grivna) {
+        return grivna/27.3;
+    }
+
+    public static double percent(double number){
+        return number*0.1+number;
+    }
+
+    public static void print(String s){
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
