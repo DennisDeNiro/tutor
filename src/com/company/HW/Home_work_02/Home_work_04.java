84289a12c79c674c15f8aa986d1167562020f18c
DennisDeNiro <popsmusics@ukr.net>
2019-03-22 10:41:40 +0200

    Init

--- src/com/company/HW/Home_work_02/Home_work_04.java
+++ src/com/company/HW/Home_work_02/Home_work_04.java
@@ -1,5 +1,7 @@
 package com.company.HW.Home_work_02;
-
+/*
+Ввести с клавиатуры три числа, вывести на экран среднее из них.
+*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,13 +23,20 @@ public class Home_work_04
         String st3 = bf.readLine();
         int c = Integer.parseInt(st3);
         // TODO: 19.03.2019  check hw
-        if (a > b && a < c) {
+        if (a > b && a < c) {           //2 1 3                     1 2 3, 1 3 2,2 1 3, 2 3 1,3 2 1, 3 1 2
             System.out.println(a);
-        } else if (b < a && b > c) {
+        } else if (b > a && b < c) {   // 1 2 3
             System.out.println(b);
-        } else if (c > a && c < b) {
+        } else if (c > a && c < b) {  //  1 3 2
+            System.out.println(c);
+        } else if (b > a && a > c) {  //  2 3 1
+            System.out.println(a);
+        }else if (a > b && b > c){     // 3 2 1
+            System.out.println(b);
+        }else if (c > b && a > c){      // 3 1 2
             System.out.println(c);
         }
+
     }
 }
 // этот код работает только при одном условии
