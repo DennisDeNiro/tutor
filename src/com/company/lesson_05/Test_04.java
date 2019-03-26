cdeb35c1886042af574713c1bfe0e360b2e4a10c
DennisDeNiro <popsmusics@ukr.net>
2019-03-26 20:51:11 +0200

    Init

---
+++ src/com/company/lesson_05/Test_04.java
@@ -0,0 +1,12 @@
+package com.company.lesson_05;
+/*
+Используя цикл for вывести на экран чётные числа от 1 до 100 включительно.
+Через пробел либо с новой строки.
+*/
+public class Test_04
+{
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i++)
+            System.out.println(i);
+    }
+}
