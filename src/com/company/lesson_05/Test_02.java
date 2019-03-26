a1e1e3bd3c88baf70cde4e470bd651b7b8e44bfb
DennisDeNiro <popsmusics@ukr.net>
2019-03-26 20:43:34 +0200

    Init

---
+++ src/com/company/lesson_05/Test_02.java
@@ -0,0 +1,15 @@
+package com.company.lesson_05;
+
+/*
+Вывести на экран числа от 1 до 10 используя цикл while.
+*/
+public class Test_02 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
