2b900555dde81e666c08a8538b43c7d40ac2bade
DennisDeNiro <popsmusics@ukr.net>
2019-03-26 21:08:04 +0200

    Init

---
+++ src/com/company/lesson_05/Test_05.java
@@ -0,0 +1,38 @@
+package com.company.lesson_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Ввести с клавиатуры строку и число N.
+Вывести на экран строку N раз используя цикл while.
+Пример ввода:
+абв
+2
+Пример вывода:
+абв
+абв
+*/
+public class Test_05 {
+    public static void main(String[] args) throws IOException {
+        int a = input();
+
+    }
+
+    public static int input() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String st = bf.readLine();
+        int number = Integer.parseInt(st);
+        String st2 = bf.readLine();
+
+        return number;
+    }
+
+    public static String a(String N) {
+        System.out.println(N);
+        return N;
+    }
+}
+
+
