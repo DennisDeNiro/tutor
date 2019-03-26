2e372bca398426bc399c33498ec7ec6db94b95be
DennisDeNiro <popsmusics@ukr.net>
2019-03-26 20:48:23 +0200

    Init

---
+++ src/com/company/lesson_05/Test_03.java
@@ -0,0 +1,15 @@
+package com.company.lesson_05;
+/* 10 чисел наоборот
+Вывести на экран числа от 10 до 1 используя цикл while.
+*/
+public class Test_03
+{
+    public static void main(String[] args) {
+
+       int a = 10;
+       while(a >= 0){
+           System.out.println(a);
+           a--;
+       }
+    }
+}
