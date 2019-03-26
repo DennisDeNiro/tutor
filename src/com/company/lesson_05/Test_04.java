b8669ccaef2ccd7bbc3c0d3d4fafb41cfff2eb1d
DennisDeNiro <popsmusics@ukr.net>
2019-03-26 20:54:48 +0200

    Init

--- src/com/company/lesson_05/Test_04.java
+++ src/com/company/lesson_05/Test_04.java
@@ -6,7 +6,8 @@ package com.company.lesson_05;
 public class Test_04
 {
     public static void main(String[] args) {
-        for(int i=0;i<=100;i++)
+        for(int i=2;i<=100; i += 2)
+
             System.out.println(i);
     }
 }
