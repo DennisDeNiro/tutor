25b5761e1ce1a0fc29845be7dadb8dbd55e4f17d
DennisDeNiro <popsmusics@ukr.net>
2019-04-02 13:57:00 +0300

    Init

--- src/com/company/HW/Home_work_06_OBJECT_CLASS_METHODS/Home_work_02.java
+++ src/com/company/HW/Home_work_06_OBJECT_CLASS_METHODS/Home_work_02.java
@@ -1,5 +1,12 @@
 package com.company.HW.Home_work_06_OBJECT_CLASS_METHODS;
 
+
+/*
+1) Создать класс Woman с параметрами: name, age. Подумай, какого типа должна быть каждая переменная.
+2) Создать классы Cat, Dog, Fish с параметрами: name, age, owner. Подумай, какого типа должна быть каждая переменная.
+3) В выполняющем методе создать по одному экземпляру каждого класса и
+   сделать так, чтобы владельцем каждого питомца была одна и таже женщина.
+*/
 public class Home_work_02 {
     public static void main(String[] args) {
         Woman woman = new Woman("Anna", 28);
