6a817f7f71e80727681a7e7d3787aa7530e6570d
DennisDeNiro <popsmusics@ukr.net>
2019-04-02 17:12:05 +0300

    Init

--- src/com/company/HW/Home_work_06_OBJECT_CLASS_METHODS/Home_work_03.java
+++ src/com/company/HW/Home_work_06_OBJECT_CLASS_METHODS/Home_work_03.java
@@ -1,5 +1,10 @@
 package com.company.HW.Home_work_06_OBJECT_CLASS_METHODS;
-
+/*
+1) Создать класс Woman с параметрами: name, age, husband. Подумай, какого типа должна быть каждая переменная.
+2) Создать класс Man с параметрами: name, age, wife. Подумай, какого типа должна быть каждая переменная.
+3) В выполняющем методе создать по одному экземпляру каждого класса и
+   сделать так, чтобы объекты стали мужем и женой.
+*/
 public class Home_work_03 {
     public static void main(String[] args) {
         Woman1 woman = new Woman1("Tanya", 43, true);
