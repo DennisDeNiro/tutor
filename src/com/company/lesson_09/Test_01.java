a26ffa35f520a51541d222a6b1119c88a2688d7c
Денис <popsmuiscs@ukr.net>
2019-04-09 20:50:31 +0300

    Init

--- src/com/company/lesson_09/Test_01.java
+++ src/com/company/lesson_09/Test_01.java
@@ -5,20 +5,20 @@ package com.company.lesson_09;
 */
 public class Test_01 {
     public static void main(String[] args) {
-        int a = 10;
         int[] array = new int[10];
-        array[0] = 0;
-        array[1] = 1;
-        array[2] = 2;
-        array[3] = 3;
-        array[4] = 4;
-        array[5] = 5;
-        array[6] = 6;
-        array[7] = 7;
-        array[8] = 8;
-        array[9] = 9;
-        for (int i =0; i<array.length; i++){
-            System.out.println(array[i]);
+
+//        array[0] = 0;
+//        array[1] = 1;
+//        array[2] = 2;
+//        array[3] = 3;
+//        array[4] = 4;
+//        array[5] = 5;
+//        array[6] = 6;
+//        array[7] = 7;
+//        array[8] = 8;
+//        array[9] = 9;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]+1);
         }
     }
 }
