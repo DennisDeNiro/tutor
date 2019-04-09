e2b77f1458b2d7613f9a022362f835a901ed3c4b
Денис <popsmuiscs@ukr.net>
2019-04-09 21:32:03 +0300

    Init

--- src/com/company/lesson_09/Test_02.java
+++ src/com/company/lesson_09/Test_02.java
@@ -1,14 +1,18 @@
 package com.company.lesson_09;
+// Заполнение массива из 10 чисел, числами от 10 до 1
 
+// [0] = 10
+// ...
+// [9] = 1
 public class Test_02 {
     public static void main(String[] args) {
         int[] array = new int[10];
 
-        for (int a = 10; a < array.length; a--) {
-            array[a] = a - 1;
-
+        for (int a = 0; a > array.length; a--) {
+            array[a] = a - 1; // array[9] = 8;
         }
-        for (int a = 0; a < array.length; a--) {
+
+        for (int a = 0; a < array.length; a++) {
             System.out.println(array[a]);
         }
     }
