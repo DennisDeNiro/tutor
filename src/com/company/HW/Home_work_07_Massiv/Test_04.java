a58e685a08555119926c53ea1e26a83b8ad030fc
Денис <popsmuiscs@ukr.net>
2019-04-11 11:35:02 +0300

    Init

--- src/com/company/HW/Home_work_07_Massiv/Test_04.java
+++ src/com/company/HW/Home_work_07_Massiv/Test_04.java
@@ -7,19 +7,16 @@ import java.io.InputStreamReader;
 // Ввести 5 чисел с клавиатуры и записать их в массив
 // Вывести содержимое массива в консоль
 public class Test_04 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 //        String st = bf.readLine();
 //        int n = Integer.parseInt(st);
-//
-//        int[] a = new int[5];
-//
-//        for (int i = 0; i < a.length; i++) {
-//            System.out.println("Индекс № "+ i +":");
-//
-//        }
-//
-//
-//    }
-
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(bf.readLine());
+        }
+        for (int i = 0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
 }
