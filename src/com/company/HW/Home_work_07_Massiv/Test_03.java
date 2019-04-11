a4ad216e72ab08a67ce4ac01421d25468f1ecd3c
Денис <popsmuiscs@ukr.net>
2019-04-11 08:31:04 +0300

    Init

--- src/com/company/HW/Home_work_07_Massiv/Test_03.java
+++ src/com/company/HW/Home_work_07_Massiv/Test_03.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // Ввести 5 строк с клавиатуры и записать их в массив
-public class Test_03
-{
+public class Test_03 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] array = new String[5];
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             array[i] = bf.readLine();
+        }
+        for (int i = 0; i < 5; i++) {
             System.out.println(array[i]);
         }
     }
