816bef23150d87aecf9c0f175e9ad3f6b2be06f4
Денис <popsmuiscs@ukr.net>
2019-04-11 18:56:08 +0300

    Init

--- src/com/company/HW/Home_work_05_FOR_EACH/Home_work_04a.java
+++ src/com/company/HW/Home_work_05_FOR_EACH/Home_work_04a.java
@@ -3,8 +3,27 @@ package com.company.HW.Home_work_05_FOR_EACH;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/* Посчитать сумму чисел
+Вводить с клавиатуры числа и считать их сумму.
+Если пользователь ввел -1, вывести на экран сумму и завершить программу.
+ -1 должно учитываться в сумме.
+*/
 public class Home_work_04a
 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader df = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(df.readLine());
+        int sum = 0;
 
+
+        if (num > 0) {
+            System.out.println("Введите числа");
+
+            sum = num + num;
+        }
+        else (num<=-1){
+            System.out.println(sum);
+
+        }
+    }
 }
