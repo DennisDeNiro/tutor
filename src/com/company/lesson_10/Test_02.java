9209f6b7dd4ab30eba383378de91fdb99cdc4d0e
Денис <popsmuiscs@ukr.net>
2019-04-15 19:17:25 +0300

    Init

--- src/com/company/lesson_10/Test_02.java
+++ src/com/company/lesson_10/Test_02.java
@@ -27,7 +27,7 @@ public class Test_02 {
     }
 
     public static void printArray(String[] arr) {  // метод вывода елеметов в обратном порядке
-//        String array = arr[0];   // принимаем первый елемент массива
+        //        String array = arr[0];   // принимаем первый елемент массива
         for (int i = 0; i <= arr.length; i++) {     // цикл for перечисляет восемь елеметов массива
         }
         System.out.println(printArray(arr[i]);
