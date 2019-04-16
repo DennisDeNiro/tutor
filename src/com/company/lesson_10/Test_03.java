26eeba9c0fc88ea78cf2c182488f1721f23b69ec
DennisDeNiro <popsmusics@ukr.net>
2019-04-16 21:01:24 +0300

    init commit

--- src/com/company/lesson_10/Test_03.java
+++ src/com/company/lesson_10/Test_03.java
@@ -7,9 +7,24 @@ package com.company.lesson_10;
  индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
  Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.
 */
+
+/*
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Stack -> primitives, local verb, obj ref   a, b=34
+    Heap -> objects  new Test_02 [[YG][OG]]  YG[[ES][SS]]  SS[[s1][s2]]
+    Garbage collector
+*/
+
 public class Test_03
 {
+    int b = 34;
     public static void main(String[] args) {
-
+        Test_02 a = new Test_02();
     }
 }
