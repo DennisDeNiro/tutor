b43d20576055c27b9def013ea896b2e42669ef4a
DennisDeNiro <popsmusics@ukr.net>
2019-04-16 21:33:28 +0300

    init commit

--- src/com/company/lesson_11/Test_02.java
+++ src/com/company/lesson_11/Test_02.java
@@ -21,10 +21,14 @@ public class Test_02 {
         int[] b = new int[5];
         int[] c = new int[5];
 
-        for (int i = 0; i<=5; i++){
+        for (int i = 0; i <= 5; i++) {
             b[i] = a[i];
-            c[i] = a[i];
             System.out.println(b[i]);
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            c[i] = a[i];
+            System.out.println(c[i]);
         }
 
 
