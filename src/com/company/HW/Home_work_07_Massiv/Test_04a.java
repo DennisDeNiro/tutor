4c8f48f837474c646a55d8fae520ef3830d785bb
DennisDeNiro <popsmusics@ukr.net>
2019-04-18 08:41:50 +0300

    init commit

---
+++ src/com/company/HW/Home_work_07_Massiv/Test_04a.java
@@ -0,0 +1,20 @@
+package com.company.HW.Home_work_07_Massiv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Ввести 5 чисел с клавиатуры и записать их в массив
+// Вывести содержимое массива в консоль
+public class Test_04a {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[5];
+        for (int i = 0; i <= a.length; i++) {
+            a[i] = Integer.parseInt(bf.readLine());
+        }
+        for (int i = 0; i<-a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ src/com/company/HW/Home_work_07_Massiv/Test_05a.java
@@ -0,0 +1,17 @@
+package com.company.HW.Home_work_07_Massiv;
+
+// Создать массив и заполнить его на 5 чисел используя быструю (статическую) инициализацию.
+// Посчитать сумму элементов массива и вывести её на экран.
+public class Test_05a {
+    public static void main(String[] args) {
+        int[] a = {1, 3, 4, 5, 6};
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+            System.out.println(sum);
+        }
+//        for (int i = 0; i < a.length; i++) {
+//
+//        }
+    }
+}
