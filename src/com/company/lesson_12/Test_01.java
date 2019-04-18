4bd37524176a5a1a2ce022ddaef72f3615d930d0
DennisDeNiro <popsmusics@ukr.net>
2019-04-18 21:22:38 +0300

    init commit

--- src/com/company/lesson_12/Test_01.java
+++ src/com/company/lesson_12/Test_01.java
@@ -12,36 +12,28 @@ public class Test_01
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int[] a = new int[5];
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < a.length; i++) {
             a[i] = Integer.parseInt(bf.readLine());
-            int b =a[0];
-            if (a[0]<b){
-                b=a[0];
-                int c = a[1];
-                if (a[1]<c){
-                    c = a[1];
-                    int d = a[2];
-                    if (a[2]<d){
-                        d = a[2];
-                        int e = a[3];
-                        if (a[3]<e){
-                            e = a[3];
-                            int f = a[4];
-                            if (a[4]<f){
-                                f = a[4];
-                            }
-                        }
-                    }
-                }
-
-            }
-
-            System.out.println(a[0]+" "+a[1]+" "+a[2]+" "+a[3]+" "+a[4]);
         }
 
-//        System.out.println(a[4]+" "+a[3]+" "+a[2]+" "+a[1]+" "+a[0]);
-//        System.out.println(a[0]+" "+a[4]+" "+a[3]+" "+a[2]+" "+a[1]);
-//        System.out.println(a[1]+" "+a[0]+" "+a[4]+" "+a[3]+" "+a[2]);
-//        System.out.println(a[4]+" "+a[0]+" "+a[1]+" "+a[2]+" "+a[3]);
+        sort(a);
+
+        for (int i : a) {
+            System.out.println(i);
+        }
     }
+
+    private static void sort(int[] arr) { // 5 3 6 1 -> 3 5 6 1 -> 3 5 1 6 -> 3 1 5 6 -> 1 3 5 6 // ... 100 103 105 104
+        for (int i = 0; i < arr.length; i++) { // i=0,i<4; 1=1 ; i=2
+            for (int j = 0; j < arr.length - 1 - i; j++) { // j=0,0<4; j=1,1<4; j=2,2<4
+                if(arr[j] > arr[j+1]){ //5 > 3; 5 > 6; 6 > 1
+                    int temp = arr[j]; //temp=5;     ; temp = 6;
+                    arr[j] = arr[j+1]; //arr[0]=3;   ; arr[2]=1;
+                    arr[j+1] = temp; //arr[1]=5;     ; arr[3]=6;
+                }
+            }
+        }
+    }
+
+
 }
