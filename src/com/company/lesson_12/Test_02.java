91b57eabd5609b2668102cf6c17b219e9cf33283
DennisDeNiro <popsmusics@ukr.net>
2019-04-18 21:47:06 +0300

    init commit

--- src/com/company/lesson_12/Test_02.java
+++ src/com/company/lesson_12/Test_02.java
@@ -9,7 +9,7 @@ import static java.util.Arrays.sort;
 public class Test_02 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int[] a = new int[50];
+        int[] a = new int[20];
         for (int i = 0; i < a.length; i++) {
             a[i] = Integer.parseInt(bf.readLine());
         }
@@ -23,11 +23,11 @@ public class Test_02 {
 
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                if (arr[j] < arr[j + 1]) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int sort = arr[j+1];
                     arr[j+1] = arr[j];
-                    arr[j+1] = sort;
+                    arr[j] = sort;
                 }
             }
         }
