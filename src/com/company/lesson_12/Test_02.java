bb000ddcd06bebfb28995252844313c67b8a5d00
DennisDeNiro <popsmusics@ukr.net>
2019-04-18 21:37:53 +0300

    init commit

---
+++ src/com/company/lesson_12/Test_02.java
@@ -0,0 +1,38 @@
+package com.company.lesson_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.util.Arrays.sort;
+
+public class Test_02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[50];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(bf.readLine());
+        }
+
+        sort(a);
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int sort = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j+1] = sort;
+                }
+            }
+        }
+    }
+}
+
+
+
