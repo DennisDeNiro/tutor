2b82ec2fcb31a983adb8b989412a49f78882fe74
DennisDeNiro <popsmusics@ukr.net>
2019-04-24 12:57:45 +0300

    init commit

--- src/com/company/HW/Home_work_08Massiv/Test_05.java
+++ src/com/company/HW/Home_work_08Massiv/Test_05.java
@@ -4,30 +4,36 @@ package com.company.HW.Home_work_08Massiv;
 // Написать метод getMax(), который вычисляет максимальное значение из массива. Ну и метод getMin() для нахождения минимального значения.
 public class Test_05 {
     public static void main(String[] args) {
+        initializarray();
+    }
+
+    public static int initializarray() {
         int[] mice = new int[]{4, 9, 3, 22, 5, 18, 25, 7, 11, 15, 5, 19};
-        initializarray(getMax());
+        getMax(mice);
+        return getMax();
+
+        getMin(mice);
+        return getMin();
+
     }
 
-    public static int initializarray(int[] array){
-
-
-    }
-
-    public static int getMax(int[] arr){
+    public static int getMax(int[] arr) {
         int max = arr[0];
-        for (int i=0; i<arr.length; i++){
-            arr[i]> max;
-            max = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
         return max;
     }
 
-    public static int getMin(int[] arr){
-        int min = arr[0];
-        for (int i =0; i<arr.length; i++){
-            arr[i]>min;
-            min = arr[i];
-        }
-        return min;
+        public static int getMin(int[] arr){
+            int min = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > min) {
+                    min = arr[i];
+                }
+            }
+            return min;
     }
 }
