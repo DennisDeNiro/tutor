4cc700444aab4ac8cded1fdda1e4ac23d8403fc6
DennisDeNiro <popsmusics@ukr.net>
2019-04-25 16:56:13 +0300

    init commit

--- src/com/company/lesson_13/Test_01.java
+++ src/com/company/lesson_13/Test_01.java
@@ -1,5 +1,7 @@
 package com.company.lesson_13;
 
+import java.util.Arrays;
+
 /**
  * Missing number
  * You are given a array of n-1 integers and these integers are in the range of 0 to n.
@@ -10,12 +12,48 @@ package com.company.lesson_13;
  */
 public class Test_01 {
     public static void main(String[] args) {
+        int[] x = new int[]{0, 1, 2, 4, 6, 3, 7, 8};
 
+        sort(x);
+        for (int i : x) {
+            System.out.println(i);
+        }
+        inNumber(x);
+        for (int i : x) {
+            System.out.println(i);
+        }
     }
 
-    public static int[] Array(){
-        int[] x = new int[]{0, 1, 2, 4, 6, 3, 7, 8};
-        return x;
+    private static void sort(int[] x) {
+
+        boolean isSorted = false;
+        int a;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < x.length - 1; i++) {
+                if (x[i] > x[i + 1]) {
+                    isSorted = false;
+                    a = x[i];
+                    x[i] = x[i + 1];
+                    x[i + 1] = a;
+                }
+
+            }
+        }
+        System.out.println(Arrays.toString(x));
+    }
+
+
+    public static void inNumber(int[] x) {
+        int min = x[0];
+        int first =x[1];
+        for (int i = 0; i < x.length-1; i++) {
+                if (min+1 == first){
+                    System.out.println(x);
+                }else if (min+1!=first){
+                    System.out.println(x[i+1]);
+                }
+        }
     }
 }
 
