1cdd4aad686cc57355d8316becaf4032a467c110
DennisDeNiro <popsmusics@ukr.net>
2019-05-10 12:21:47 +0300

    init commit

---
+++ src/com/company/lesson_15/Test_03.java
@@ -0,0 +1,47 @@
+package com.company.lesson_15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Map;
+
+/* Удвой слова
+1. Введи с клавиатуры 5 слов в список строк.
+2. Метод doubleValues должен удваивать слова по принципу a,b,c -> a,a,b,b,c,c.
+3. Используя цикл for выведи результат на экран, каждое значение с новой строки.
+
+Пример:
+краб
+лось
+рак
+
+Выходные данные:
+краб краб
+лось лось
+рак рак
+*/
+public class Test_03 {
+    public static void main(String[] args) throws IOException {
+        System.out.print(doubleValues(EnterLetter()));
+    }
+
+    private static ArrayList<String> EnterLetter() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 3; i++) {
+            String s = bf.readLine();
+            list.add(s);
+        }
+        return list;
+    }
+
+    private static ArrayList<String> doubleValues(ArrayList<String> list) {
+        for (int i = 0; i < list.size();) {
+            list.add(i , list.get(i));
+            i+=2;
+        }
+        return list;
+    }
+}
+
