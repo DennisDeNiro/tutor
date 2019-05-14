536d40c1480bd04c78e446ec4929dfc3c5e13995
DennisDeNiro <popsmusics@ukr.net>
2019-05-14 21:59:55 +0300

    init commit

--- src/com/company/lesson_17/Test_03.java
+++ src/com/company/lesson_17/Test_03.java
@@ -1,4 +1,33 @@
 package com.company.lesson_17;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Вводить с клавиатуры строки, пока пользователь не введёт строку 'end'
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введёт строку "end".  "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+*/
 public class Test_03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = new ArrayList<>();
+
+        while (true) {
+            String s = bf.readLine();
+            list.add(s);
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).contains("end")) {
+                    list.remove(list.get(i));
+                    break;
+                }
+            }
+        }
+
+
+    }
 }
