14c0b613aaa6857790bfb5e2d4364db606abbd91
DennisDeNiro <popsmusics@ukr.net>
2019-05-16 21:23:28 +0300

    init commit

--- src/com/company/lesson_18/Test_04.java
+++ src/com/company/lesson_18/Test_04.java
@@ -1,4 +1,14 @@
 package com.company.lesson_18;
 
+import java.util.Date;
+
+/* Получение текущей даты
+Посчитать сколько лет прошло с 1970 года */
 public class Test_04 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getYear());
+        System.out.println(date.getYear()-70);
+    }
 }
---
+++ src/com/company/lesson_18/Test_05.java
@@ -0,0 +1,15 @@
+package com.company.lesson_18;
+
+import java.util.Date;
+
+// Вычислить разницу между двумя датами
+public class Test_05 {
+    public static void main(String[] args) throws InterruptedException {
+        Date date = new Date();
+        Thread.sleep(6000);
+        Date date1 = new Date();
+        System.out.println(date);
+        System.out.println(date1);
+
+    }
+}
