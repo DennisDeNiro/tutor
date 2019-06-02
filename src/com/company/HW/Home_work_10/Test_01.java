b7748581f92e7c8c180ae8c1db5884db58746886
DennisDeNiro <popsmusics@ukr.net>
2019-06-02 10:52:28 +0300

    init commit

--- src/com/company/HW/Home_work_10/Test_01.java
+++ src/com/company/HW/Home_work_10/Test_01.java
@@ -2,6 +2,7 @@ package com.company.HW.Home_work_10;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 /* Измерить сколько времени занимает 10 тысяч вызовов get для каждого списка
@@ -12,8 +13,10 @@ import java.util.List;
 //почему когда явно указываеться тип ощыбка
 public class Test_01 {
     public static void main(String[] args) {
-        List list = new ArrayList<>();
+        List list = new ArrayList();
+        List list2 = new LinkedList();
         System.out.println(getTimeMsOfGet(fill(list)));
+        System.out.println(getTimeMsOfGet(fill(list2)));
     }
 
     public static List fill(List list) {
