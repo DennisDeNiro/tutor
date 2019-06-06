d181ac2e2190b75008b71e900711ab545360d796
DennisDeNiro <popsmusics@ukr.net>
2019-06-06 21:06:57 +0300

    init commit

---
+++ src/com/company/lesson_22/Test_02.java
@@ -0,0 +1,9 @@
+package com.company.lesson_22;
+/* Задача по алгоритмам. Фиббоначи
+   Найти ряд Фиббоначи для 10
+*/
+public class Test_02 {
+    public static void main(String[] args) {
+
+    }
+}
