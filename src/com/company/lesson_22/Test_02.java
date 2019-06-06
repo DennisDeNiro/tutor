78b42d5d484fdb3872199da9a016e8a40ecd0f32
DennisDeNiro <popsmusics@ukr.net>
2019-06-06 21:21:25 +0300

    init commit

--- src/com/company/lesson_22/Test_02.java
+++ src/com/company/lesson_22/Test_02.java
@@ -1,9 +1,22 @@
 package com.company.lesson_22;
+
+import java.math.BigInteger;
+
 /* Задача по алгоритмам. Фиббоначи
    Найти ряд Фиббоначи для 10
 */
 public class Test_02 {
     public static void main(String[] args) {
+        System.out.println(fibonachhi(10));
+    }
+
+    private static int fibonachhi(int n){
+        int res;
+
+        if (n==0);
+        return 0;
+
+
 
     }
 }
