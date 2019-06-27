4e33cea5a8cbd7f6a6ba179f8e96cccf8f921e25
DennisDeNiro <popsmusics@ukr.net>
2019-06-27 13:46:35 +0300

    init commit

---
+++ src/com/company/HW/Home_work_11/Home_work_01.java
@@ -0,0 +1,43 @@
+package com.company.HW.Home_work_11;
+/*
+1. Создать класс Cat с переменными name, age, weight.
+2. Скрыть все внутренние переменные класса Cat:
+- создать геттеры и сеттеры
+- скрыть методы, позволяющие менять внутреннее состояние объектов класса Cat.
+*/
+public class Home_work_01 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+    }
+}
+
+class Cat{
+    private String name;
+    private int age;
+    private int weight;
+
+    private String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    private int getWeight() {
+        return weight;
+    }
+
+    private void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
